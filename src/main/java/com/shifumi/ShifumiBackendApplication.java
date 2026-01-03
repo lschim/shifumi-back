d88dfac95ec8f2dbf9d93d583d8b0c587ae291b2
Louis Schimchowitsch <schim.louis@gmail.com>
2026-01-03 14:23:17 +0100

    chore: Initial commit of the skeleton

---
+++ src/main/java/com/shifumi/ShifumiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shifumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifumiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifumiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shifumi/ShifumiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shifumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifumiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
